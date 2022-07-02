d4845b014cebf4d52336d8db37f6221d60cadbea
Deya <deya@Octopus.local>
2022-07-02 19:54:20 +0300

    interpolation with unit tests

--- Sources/Cascaliana/Cascaliana.swift
+++ Sources/Cascaliana/Cascaliana.swift
@@ -1,6 +1,25 @@
 public struct Cascaliana {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
+    
+    enum InterpolationError: Error {
+        case invalidInput
     }
+    
+    func interpolate<T:BinaryFloatingPoint> (target: T, x:[T], y:[T], n:Int = 100) throws -> T {
+        
+        guard !x.isEmpty, !y.isEmpty else { throw InterpolationError.invalidInput }
+        
+        if target <= x[0] { return y[0] }
+        for i in 1...n {
+            if target <= x[i] {
+                let ans = (target-x[i-1]) * (y[i] - y[i-1]) / (x[i]-x[i-1]) + y[i-1]
+                return ans
+            }
+        }
+        return y[n]
+    }
+    
+    public init() {
+        
+    }
+    
 }
--- Tests/CascalianaTests/CascalianaTests.swift
+++ Tests/CascalianaTests/CascalianaTests.swift
@@ -2,10 +2,30 @@ import XCTest
 @testable import Cascaliana
 
 final class CascalianaTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Cascaliana().text, "Hello, World!")
+    
+    func testInterpolateEmptyArrays() throws {
+        let sut = Cascaliana()
+
+        let original_times:[Double] = []
+        let original_values:[Double] = []
+        
+        let ipol = try? sut.interpolate(target: 0, x: original_times, y: original_values)
+        print(ipol)
+        XCTAssertNil(ipol)
+        
     }
+    
+    func testInterpolate0to100() throws {
+        let sut = Cascaliana()
+
+        let original_times:[Double] = [0.0, 10.0]
+        let original_values:[Double] = [0.0, 100]
+        
+        let ipol = try? sut.interpolate(target: 7, x: original_times, y: original_values)
+        print(ipol)
+        
+        XCTAssert(ipol == 70)
+        
+    }
+    
 }
